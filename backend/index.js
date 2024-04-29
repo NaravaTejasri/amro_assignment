9d9fd9030f4776be58ae23ab9372aaef9c332483
NaravaTejasri <tejasrisony17@gmail.com>
2024-04-29 15:07:15 +0200

    created app and  added imports

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,17 @@ const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
 
+//imports
+const nodeRouter = require("./routers/nodeRouter");
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 8000;
+app.use(nodeRouter);
+
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
